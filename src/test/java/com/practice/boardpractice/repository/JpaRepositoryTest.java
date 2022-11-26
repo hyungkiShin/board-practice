6590cf58946e3677d3b092f77e5c53a6a4a09c38
hyungkiShin <tkaqkeldk@nate.com>
2022-11-26 20:47:09 +0900

    [feat] #1 - Spring Data Rest 테스트 제외 처리
    이것은 data rest 의 기능이고 통합테스트라 무거우며 db 에도 영향을 준다.
    떄문에 삭제하지는 않고 제외 처리해서 전체 테스트 중에 실행되지 않게끔 처리

--- src/test/java/com/practice/boardpractice/repository/JpaRepositoryTest.java
+++ src/test/java/com/practice/boardpractice/repository/JpaRepositoryTest.java
@@ -17,6 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 
+
 @ActiveProfiles("testdb")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @DisplayName("Jpa 연결 테스트")
