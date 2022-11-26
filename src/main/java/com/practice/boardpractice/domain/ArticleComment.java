54e816ddf559af004a4242956b2f7ec4dbb86014
hyungkiShin <tkaqkeldk@nate.com>
2022-11-26 17:49:05 +0900

    [feat] #8 도메인 설계 및 접근로직 테스트 정의
    data.sql 파일에 테스트 데이터 정의함. mockaroo 라는 좋은 사이트가 있었다..
    entity 연관관계를 되짚어보았다. 팀내에서 오히려 이런 부분들 (연관관계 매핑) 을 안하는 조직도 있다고 하는데,
    안하는 이유는 운영 데이터 의 보존, 유지보수, 퍼포먼스 때문이다.
    
    @DataJpaTest 를 통해 jpa 를 테스트 하였다. 엔티티Repository.saveAndFlush saveAndFlush 는 처음알았다.
    tdd 방식에 이어서, github 에 스플린트작성후, 단위별로 커밋하니 맛만좋다

--- src/main/java/com/practice/boardpractice/domain/ArticleComment.java
+++ src/main/java/com/practice/boardpractice/domain/ArticleComment.java
@@ -1,15 +1,83 @@
 package com.practice.boardpractice.domain;
 
-import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "content"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class ArticleComment {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Setter @ManyToOne(optional = true)
     private Article article; // 게시글 (ID)
+
+    @Setter @Column(nullable = false, length = 500)
     private String content; // 본문
 
-    private LocalDateTime createdAt; // 생성일시
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt; // 생성일자
+
+    @CreatedBy
+    @Column(nullable = false, length = 100)
     private String createdBy; // 생성자
-    private LocalDateTime modifiedAt; // 수정일시
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt; // 수정일자
+
+    @LastModifiedBy
+    @Column(nullable = false, length = 100)
     private String modifiedBy; // 수정자
+
+    protected ArticleComment() {}
+
+    private ArticleComment(final Article article, final String content) {
+        this.article = article;
+        this.content = content;
+    }
+
+    public static ArticleComment of(final Article article, final String content) {
+        return new ArticleComment(article, content);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ArticleComment that)) return false;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
